31810b65eda7efed5bbf5610fff5f2029377b122
Luciana Lemos <luciana.lemos@sjt.edu.br>
2024-01-15 20:21:16 -0300

    initial commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Luciana Lemos on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
